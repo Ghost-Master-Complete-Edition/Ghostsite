82e5d187c40a0783ea0fb22f8ec367f04e06cf07
Justaduck21 <surdudario122@gmail.com>
2026-03-17 15:58:03 +0100

    commented out cursor glow for testing

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
-//add API keys
+//add API key here for testing locally
 
 export const environment = {
     YOUTUBE_API_KEY: ""
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,3 @@
-//add API keys
-
 export const environment = {
     YOUTUBE_API_KEY: process.env["YOUTUBE_API_KEY"]
 };
