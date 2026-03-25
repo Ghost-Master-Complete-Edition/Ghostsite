87b96f69276526079eff847f5b0b2b7bf0234d45
Justaduck21 <surdudario122@gmail.com>
2026-03-25 19:23:18 +0100

    Update plugins.ts

--- strapi-backend/config/plugins.ts
+++ strapi-backend/config/plugins.ts
@@ -1,12 +1,10 @@
 import type { Core } from '@strapi/strapi';
-import * as crypto from 'crypto';
+const crypto = require('crypto');
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
 'users-permissions': {
     config: {
-      jwt: {
-        jwtSecret: env('STRAPI_JWT_SECRET') || crypto.randomBytes(16).toString('base64')
-      },
+        jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
     },
 },
 }
