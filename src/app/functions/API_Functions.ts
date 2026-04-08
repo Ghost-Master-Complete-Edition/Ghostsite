aa53f72cc191384c84612ec0fe9f0bca5cc45b37
Justaduck21 <surdudario122@gmail.com>
2026-04-08 12:24:15 +0200

    Update API_Functions.ts

--- src/app/functions/API_Functions.ts
+++ src/app/functions/API_Functions.ts
@@ -2,9 +2,11 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     const value = process.env["YOUTUBE_API_KEY"];
-    console.log(value);
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+        },
         body: JSON.stringify({ message: { value } }),
     };
 };
