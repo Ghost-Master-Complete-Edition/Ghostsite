f66fcb7d77db0ac53f1d31c64983c5652879d373
Justaduck21 <surdudario122@gmail.com>
2026-05-14 19:03:49 +0200

    added strapi api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     YOUTUBE_API_KEY: process.env["NG_APP_YOUTUBE_API_KEY"],
     STRAPI_LINK: process.env["NG_APP_STRAPI_LINK"],
+    STRAPI_API_KEY: process.env["NG_APP_STRAPI_API_KEY"]
 };
